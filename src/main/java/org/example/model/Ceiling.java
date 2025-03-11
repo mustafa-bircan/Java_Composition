1a0390b45566c3ae3be21e02a597175831a11ba0
mustafa-bircan <msbrtfcn@gmail.com>
2025-03-11 10:54:42 +0300

    Update Ceiling.java

--- src/main/java/org/example/model/Ceiling.java
+++ src/main/java/org/example/model/Ceiling.java
@@ -1,4 +1,25 @@
 package org.example.model;
 
+import org.example.model.enums.PaintColor;
+
 public class Ceiling {
+    private int height;
+    private PaintColor paintColor;
+
+    public Ceiling(int height, PaintColor paintColor) {
+        this.height = height;
+        this.paintColor = paintColor;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public PaintColor getPaintColor() {
+        return paintColor;
+    }
+
+    public void create() {
+        System.out.println("Ceiling has been built.");
+    }
 }
