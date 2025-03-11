265edc4aa048a1c9b3d3b50b5e20773bd8b2a465
mustafa-bircan <msbrtfcn@gmail.com>
2025-03-11 09:58:50 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import org.example.model.Lamp;
+import org.example.model.enums.LampType;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Lamp lamp = new Lamp(LampType.NEON, true, 75);
+
+        lamp.turnOn();
+        System.out.println("Style: " + lamp.getStyle());
+        System.out.println("Battery: " + lamp.isBattery());
+        System.out.println("Glob Rating: " + lamp.getGlobRating());
     }
 }
